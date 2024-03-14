7679ca8890d8cd824aab03320da4a32158efced4
Lioz Akirav <lioz.akirav@gmail.com>
2024-03-14 04:40:32 +0200

    add docker-compose

--- python-app/app.py
+++ python-app/app.py
@@ -1,12 +1,27 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 
 app = Flask(__name__)
 
+# Get the directory path of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+log_file = os.path.join(script_dir, 'logs/app.log')
+
+# Configure logging to write to the log file
+handler = RotatingFileHandler(log_file, maxBytes=10000, backupCount=1)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
+
 
 @app.route('/')
 def hello():
+    app.logger.info('Endpoint accessed: /')
     return 'Hello, World!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8001)
